e3065dcc375d09baf224e1f95cd5a1a9d2228971
Claudia Tomassetti <hagol.cker@gmail.com>
2021-06-23 15:24:17 +0200

    finishes all exercises

--- challenge.js
+++ challenge.js
@@ -1,20 +1,30 @@
-// Challenge ex. 1
+// Challenge ex. 1 ------------------------------------------------
 
 var arr = [ 
     
-    ["Vienna"], 
-    ["<br>", "      ", "is", 9, 4, 0], 
-    ["<br>","<br>", "a", 27, 5], 
+    ["Vienna", 2, 1, 24, 2], 
+    [8, "is", 9, 4, 0], 
+    [7, 0, "a", 27, 5], 
     [7, 4, 28, "nice", 0], 
     [3, 10, 25, 7, "city"], 
 
 ]
 
-document.write(arr);
+var diagonal = [arr[0][0], arr[1][1], arr[2][2], arr[3][3], arr[4][4]];
 
-// var diagonal = [arr[0][0], arr[1][1], arr[2][2], arr[3][3], arr[4][4]];
+console.log(diagonal);
+document.write(diagonal.join(" "))
+// document.write(diagonal.join("<br>"))
 
 
+document.write("<br><br>"); // now is not ugly!! <3
 
-// console.log(diagonal);
-// document.write(diagonal.join("<br>"));
+// Challenge ex. 2 ------------------------------------------------
+
+
+var text_ = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+
+
+document.write(text_.split("$") .join(" "));
+
+
---
+++ extra.js
@@ -0,0 +1,18 @@
+// extra 1
+
+var text_ = "Hey there, i am a javascript developer, and i live in vienna";
+
+
+document.write(text_.slice(11, 38));
+
+
+//extra 2
+
+
+
+
+var fruits = ["apple","banana","kiwi"];
+console.log(fruits);
+
+fruits.splice(2,0,"orange", "strawberry");
+console.log(fruits);
