3251eacbb79235bbf4fb6331c2894df8b2ca8704
Claudia Tomassetti <hagol.cker@gmail.com>
2021-06-23 13:11:42 +0200

    adds finished ex. basic, intermediate and advanced, + draft of ex. challenge

---
+++ challenge.js
@@ -0,0 +1,20 @@
+// Challenge ex. 1
+
+var arr = [ 
+    
+    ["Vienna"], 
+    ["<br>", "      ", "is", 9, 4, 0], 
+    ["<br>","<br>", "a", 27, 5], 
+    [7, 4, 28, "nice", 0], 
+    [3, 10, 25, 7, "city"], 
+
+]
+
+document.write(arr);
+
+// var diagonal = [arr[0][0], arr[1][1], arr[2][2], arr[3][3], arr[4][4]];
+
+
+
+// console.log(diagonal);
+// document.write(diagonal.join("<br>"));
---
+++ intermediate.js
@@ -0,0 +1,85 @@
+// INTERMEDIATE EX.1
+
+// var cars = ["Tesla", "Audi", "Reanault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+
+// console.log(cars.sort());
+
+var fruits = ["apple", "banana", "kiwi"];
+
+fruits.push("orange");
+
+console.log(fruits);
+
+fruits.pop();
+
+console.log(fruits);
+
+
+
+// INTERMEDIATE EX.2
+
+var animals =["monkey", "horse", "dog"], x=0, y=2;
+console.log(animals);
+
+
+/*INTERMEDIATE SOLUTION 1 ---
+
+// animals[x] = animals.splice(y, 1, animals[x])[0];
+// console.log(animals); 
+animals.unshift("cat");
+console.log(animals);
+End ----INTERMEDIATE SOLUTION 3*/
+
+
+// INTERMEDIATE SOLUTION 2
+
+var a = animals[2];
+var b = animals [0];
+animals= [a, animals[1], b];
+
+console.log(animals);
+
+animals.unshift("cat");
+
+console.log(animals);
+
+
+/*INTERMEDIATE SOLUTION 3 ----
+
+var a = animals[2];
+var b = animals [0];
+animals= [animals[2], animals[1], animals[0]];
+console.log(animals);
+
+// animals.slice(0, 2);
+
+// animals.splice(1, 2, "dog");
+// console.log(animals); 
+animals.unshift("cat");
+console.log(animals);
+
+End ----INTERMEDIATE SOLUTION 4 */
+
+/*INTERMEDIATE SOLUTION 3 ----
+animals.pop();
+animals.shift();
+
+console.log(animals);
+
+animals.push("monkey");
+animals.unshift("dog");
+console.log(animals);
+
+animals.unshift("cat");
+
+console.log(animals);
+
+End ----INTERMEDIATE SOLUTION 4 */
+
+
+// INTERMEDIATE EX.3
+
+var fruits2 = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+
+document.write(fruits2.split("/").join("<br>"));
+
